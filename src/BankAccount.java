acfbaf1e3846da93977909d70befe7bf9d2ea6a1
Shivam Gupta <shivamaggarwal767@gmail.com>
2023-06-04 16:40:40 +0530

    all implementation

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -4,4 +4,9 @@ public interface BankAccount {
     // add money
     // check roi
     // get final interest
-}
+    Boolean withdrawMoney(int money);
+    Boolean addMoney(int money);
+    int checkBalance();
+    int getROI();
+    int getTotalInterest(int amount , int time);
+}  
